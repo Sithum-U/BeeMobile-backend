0d23417f63c4d6f903eb690fbcfaf2ab8a63f0a8
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-08-30 15:26:51 +0530

    stripe recognition

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51LcQzEELWo6Wp4wPSYTRPXIneu2cKSzuoGAZosJvBjETKDHCvnBAbToAj1wWGFeekVGCTlUJRYa7giKprhNL1koT00KxeaFnvw"
+);
+
+const uuid = require("uuid");
 
 require("dotenv").config();
 const app = express();
@@ -13,6 +18,10 @@ app.use(express.urlencoded());
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.get("/ge", (req, res) => {
+  res.send("SA Assignment");
+});
 
 mongoose
   .connect(process.env.MONGO_URL, {
