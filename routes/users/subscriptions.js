be56bb098cbdce2f393ce187f1c2f556499efb78
Sithum-U <avsithum@gmail.com>
2022-09-27 10:43:43 +0530

    Subscription routes created

---
+++ routes/users/subscriptions.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { createSubscription, updateSubscription, deleteSubscription, getSubscription, getSubscriptions } = require("../../controllers/users/subscription");
+const { verifyToken, verifyUser, verifyAdmin } = require("../../utils/verifyToken");
+
+const router = express.Router();
+
+//CREATE
+router.post("/:userId", verifyUser, createSubscription);
+
+//UPDATE
+router.put("/:id", verifyUser, updateSubscription);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteSubscription);
+
+//GET
+router.get("/:id", verifyUser, getSubscription);
+
+//GETALL
+router.get("/", verifyUser, getSubscriptions);
+
+module.exports = router;
