402208967b640b5413211c56458eab168433efcc
Sithum-U <avsithum@gmail.com>
2022-09-27 10:35:57 +0530

    Subscription controller created

--- controllers/users/user.js
+++ controllers/users/user.js
@@ -1,45 +1,46 @@
 const User = require("../../models/users/User");
+const { createError } = require("../../utils/error");
 
 const updateUser = async (req, res, next) => {
-    try{
+    try {
         const updatedUser = await User.findByIdAndUpdate(
-            req.params.id, 
-            { $set: req.body }, 
+            req.params.id,
+            { $set: req.body },
             { new: true }
         );
         res.status(200).json(updatedUser);
-    }catch (err){
+    } catch (err) {
         next(err);
     }
 }
 
 const deleteUser = async (req, res, next) => {
-    try{
+    try {
         await User.findByIdAndDelete(
             req.params.id
         );
         res.status(200).json("User has been deleted");
-    }catch (err){
+    } catch (err) {
         next(err);
     }
 }
 
 const getUser = async (req, res, next) => {
-    try{
+    try {
         const user = await User.findById(
             req.params.id
         );
         res.status(200).json(user);
-    }catch (err){
+    } catch (err) {
         next(err);
     }
 }
 
 const getUsers = async (req, res, next) => {
-    try{
+    try {
         const users = await User.find();
         res.status(200).json(users);
-    }catch (err){
+    } catch (err) {
         next(err);
     }
 }
