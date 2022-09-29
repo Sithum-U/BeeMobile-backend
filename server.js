fe112524fbc1ab0147bc853ecb8ae59dfe22dd3a
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-09-29 16:22:55 +0530

    server updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cookieParser = require("cookie-parser");
 // const sequelize = require("sequelize");
 const productRoute = require("./routes/products/productRoute");
 const paymentRoute = require("./routes/payments/paymentRoute");
+const cartItemRoute = require("./routes/cartItems/cartItemRoute");
 const authRoute = require("./routes/users/auth");
 const usersRoute = require("./routes/users/users");
 const subscriptionsRoute = require("./routes/users/subscriptions");
@@ -53,6 +54,7 @@ app.get("/", (req, res) => {
 
 app.use("/product", productRoute);
 app.use("/rate", ratesRoute);
+app.use("/cartItem", cartItemRoute);
 app.use("/payment", paymentRoute);
 app.use("/auth", authRoute);
 app.use("/users", usersRoute);
