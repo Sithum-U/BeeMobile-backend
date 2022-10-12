63069bace1cd5de39bba4325baf1a50156712e5a
Buddhini-123 <buddhiniup123@gmail.com>
2022-10-12 18:51:54 +0530

    conflicts resolved

--- models/users/User.js
+++ models/users/User.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    subscriptions: {
-      type: [String],
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
-=======
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
@@ -53,7 +23,6 @@ const userSchema = new mongoose.Schema({
         default: false
     },
 }, { timestamps: true });
->>>>>>> a0b2302f5bd37f528b56261fb8a71414148ec085
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
